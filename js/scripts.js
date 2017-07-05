3b0049577e865455481304ee13febab40b4a21d3
Esti Shay and Kate Byars <esti@>
2017-07-05 10:13:49 -0700

    add additional terms and definitions as well as changing fadeToggle to toggle.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,97 @@
 $(document).ready(function() {
   $(".js").click(function() {
-    $(".js-showing").fadeToggle();
-    $(".js-hidden").fadeToggle();
+    $(".js-showing").toggle();
+    $(".js-hidden").toggle();
   });
+
+  $(".Operators").click(function() {
+    $(".Operators-showing").toggle();
+    $(".Operators-hidden").toggle();
+  });
+
+  $(".Variables").click(function() {
+    $(".Variables-showing").toggle();
+    $(".Variables-hidden").toggle();
+  });
+
+  $(".Variablenc").click(function() {
+    $(".Variablenc-showing").toggle();
+    $(".Variablenc-hidden").toggle();
+  });
+
+  $(".Functions").click(function() {
+    $(".Functions-showing").toggle();
+    $(".Functions-hidden").toggle();
+  });
+
+  $(".Methods").click(function() {
+    $(".Methods-showing").toggle();
+    $(".Methods-hidden").toggle();
+  });
+
+  $(".Arguments").click(function() {
+    $(".Arguments-showing").toggle();
+    $(".Arguments-hidden").toggle();
+  });
+
+  $(".Parameters").click(function() {
+    $(".Parameters-showing").toggle();
+    $(".Parameters-hidden").toggle();
+  });
+
+  $(".Return").click(function() {
+    $(".Return-showing").toggle();
+    $(".Return-hidden").toggle();
+  });
+
+  $(".Chainingmethods").click(function() {
+    $(".Chainingmethods-showing").toggle();
+    $(".Chainingmethods-hidden").toggle();
+  });
+
+  $(".Strings").click(function() {
+    $(".Strings-showing").toggle();
+    $(".Strings-hidden").toggle();
+  });
+
+  $(".Booleans").click(function() {
+    $(".Booleans-showing").toggle();
+    $(".Booleans-hidden").toggle();
+  });
+
+  $(".Undefined").click(function() {
+    $(".Undefined-showing").toggle();
+    $(".Undefined-hidden").toggle();
+  });
+
+  $(".NaN").click(function() {
+    $(".NaN-showing").toggle();
+    $(".NaN-hidden").toggle();
+  });
+
+  $(".Escape").click(function() {
+    $(".Escape-showing").toggle();
+    $(".Escape-hidden").toggle();
+  });
+
+  $(".Alert").click(function() {
+    $(".Alert-showing").toggle();
+    $(".Alert-hidden").toggle();
+  });
+
+  $(".Comments").click(function() {
+    $(".Comments-showing").toggle();
+    $(".Comments-hidden").toggle();
+  });
+
+  $(".jQuery").click(function() {
+    $(".jQuery-showing").toggle();
+    $(".jQuery-hidden").toggle();
+  });
+
+  $(".Attributes").click(function() {
+    $(".Attributes-showing").toggle();
+    $(".Attributes-hidden").toggle();
+  });
+
 });
